8ab4624a992f35080c3fb0f241c206961273cf6f
alainfranck <franckalain.dev@gmail.com>
2024-02-22 14:22:11 +0000

    static files

--- economie/settings.py
+++ economie/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'economie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,9 +135,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS = [BASE_DIR / "static"]  
+#STATIC_ROOT = BASE_DIR / "staticfiles"
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]  
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    'economie/static'
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
