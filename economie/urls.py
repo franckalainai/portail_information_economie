23ad05c434aef6623b22d058a81d5a89cb4699ba
alainfranck <franckalain.dev@gmail.com>
2024-02-23 21:33:22 +0000

    allowed host 4

--- economie/urls.py
+++ economie/urls.py
@@ -4,12 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
     path('', include('core.urls')),
     path('actualites/', include('actualite.urls')),
     path('publications/', include('publication.urls')),
     path('summernote/', include('django_summernote.urls')), 
-
+    path('admin/', admin.site.urls), 
 ] 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
